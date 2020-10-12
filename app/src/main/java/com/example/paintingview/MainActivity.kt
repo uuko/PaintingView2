fb043f9eb94b017f1cd3609260bf72528f72ce50
uuko <omkkqq@gmail.com>
2020-10-12 16:20:15 +0800

    "V1.0.2"

--- app/src/main/java/com/example/paintingview/MainActivity.kt
+++ app/src/main/java/com/example/paintingview/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.paintingview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.paintingview2.PPView
+import com.example.paintingview2.PaintItActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var it=Intent(this,PaintItActivity::class.java)
+        startActivity(it);
     }
 }
